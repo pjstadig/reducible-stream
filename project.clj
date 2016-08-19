9cc4238d8ae74cef074347fea6a9d418b8d35bfe
Paul Stadig <paul@stadig.name>
2016-08-19 12:56:58 -0400

    Add clojars as the deploy repository.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/pjstadig/reducible-stream"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[com.cognitect/transit-clj "0.8.288"]]}})
